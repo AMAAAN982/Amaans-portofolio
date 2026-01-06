2c4799cfcdc0fc2d640e5c1dde39153bffd9f2b4
Amaan <amaan@example.com>
2026-01-06 22:26:07 +0500

    Initial commit

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-let nav=document.querySelector(".navbar");
-window.onscroll=function () {
-    if( document.documentElement.scrollTop > 20){
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
         nav.classList.add("header-scrolled")
+        console.log("scroll working")
 
-    }else{
+    } else {
         nav.classList.remove("header-scrolled")
     }
-
-    
 }
